a5be523810c6601eab74566a15ada1829ba9d68f
Gaëtan H <gaetanhus@gmail.com>
2025-04-15 19:38:42 +0200

    feat: Simplify IndexPageController by removing ID parameter and updating route

--- src/Controller/IndexPageController.php
+++ src/Controller/IndexPageController.php
@@ -7,11 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexPageController extends AbstractController {
-  #[Route('/index/{id}', name: 'app_index_page')]
-  public function index(string $id): Response {
-    return $this->render('index_page/index.html.twig', [
-      'controller_name' => 'IndexPageController',
-      'id' => $id
-    ]);
+  #[Route('/', name: 'app_index_page')]
+  public function index(): Response {
+    return $this->render('index_page/index.html.twig', []);
   }
 }
