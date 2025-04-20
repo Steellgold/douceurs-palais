9f45a17ec33f5bfd79d8c2d13c7d90548f99e7b9
Gaëtan H <gaetanhus@gmail.com>
2025-04-20 23:44:31 +0200

    chore(php.xml): add PhpCodeSniffer, PhpStan, and Psalm configurations to enhance code quality tools integration style(LoginFormAuthenticator.php): format constructor for better readability by adding a newline after the constructor definition

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -21,7 +21,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator {
 
   public const LOGIN_ROUTE = 'app_login';
 
-  public function __construct(private UrlGeneratorInterface $urlGenerator) { }
+  public function __construct(private UrlGeneratorInterface $urlGenerator) {
+  }
 
   public function authenticate(Request $request): Passport {
     $email = $request->request->get('email', '');
