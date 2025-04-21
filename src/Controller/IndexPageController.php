1259257a965987ddd08d9514b3fb908bbf3d1e03
Gaëtan H <gaetanhus@gmail.com>
2025-04-21 13:11:44 +0200

    feat: implement favorites page with bakery and product listings

--- src/Controller/IndexPageController.php
+++ src/Controller/IndexPageController.php
@@ -2,13 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\BakeryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexPageController extends AbstractController {
+  public function __construct(
+    private readonly BakeryRepository $bakeryRepository,
+    private readonly ProductRepository $productRepository
+  ) {
+  }
+
   #[Route('/', name: 'app_index_page')]
-  public function index(): Response { 
+  public function index(): Response {
+    $user = $this->getUser();
+
+    if ($user) {
+      $favoriteCount = $this->bakeryRepository->countFavoritesByUser($user);
+
+      if ($favoriteCount > 0) {
+        $favoriteBakeries = $this->bakeryRepository->findFavoritesByUser($user, null); // fuck php
+        $productsFromFavorites = $this->productRepository->findFromFavoriteBakeries($user, 12);
+
+        return $this->render('index_page/favorites.html.twig', [
+          'favoriteBakeries' => $favoriteBakeries,
+          'products' => $productsFromFavorites
+        ]);
+      }
+    }
+
     return $this->render('index_page/index.html.twig', []);
   }
 }
