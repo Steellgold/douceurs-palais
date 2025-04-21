35ec44118c26d6451ec8de556d7abda2429f639d
Gaëtan H <gaetanhus@gmail.com>
2025-04-21 18:12:46 +0200

    feat(Migration): create category table and update product schema for category association

---
+++ migrations/Version20250421155733.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250421155733 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE category (id VARCHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, position INT DEFAULT 0 NOT NULL, image VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', UNIQUE INDEX UNIQ_64C19C1989D9B62 (slug), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD category_id VARCHAR(36) DEFAULT NULL, DROP category
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD12469DE2
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE category
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_D34A04AD12469DE2 ON product
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD category VARCHAR(255) DEFAULT NULL, DROP category_id
+        SQL);
+    }
+}
