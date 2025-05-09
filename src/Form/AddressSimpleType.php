cd0f3b334a93a184fc94119b37145b44d546d836
Gaëtan H <gaetanhus@gmail.com>
2025-05-09 17:29:37 +0200

    feat(addresses): remove primary address checkbox from address form

--- src/Form/AddressSimpleType.php
+++ src/Form/AddressSimpleType.php
@@ -7,11 +7,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
-class AddressType extends AbstractType {
+class AddressSimpleType extends AbstractType {
   public function buildForm(FormBuilderInterface $builder, array $options): void {
     $builder
       ->add('label', TextType::class, [
