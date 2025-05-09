6fd987121ed2f5fe5214af0c352e45cd9678ec92
Gaëtan H <gaetanhus@gmail.com>
2025-05-09 17:29:26 +0200

    feat(addresses): enhance address management with primary address indication and deletion confirmation

--- src/Twig/Components/ProductCard.php
+++ src/Twig/Components/ProductCard.php
@@ -6,5 +6,12 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('ProductCard')]
 class ProductCard {
-
+  public string $name;
+  public ?string $description = null;
+  public ?string $imageUrl = null;
+  public ?string $category = null;
+  public ?float $price = null;
+  public ?string $productId = null;
+  public ?string $bakeryId = null;
+  public string $variant = 'default';
 }
